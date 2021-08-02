138d09c9611051e4666b255ccbadbd7fcb8eed44
Shadmanulislam11 <58678589+Shadmanulislam11@users.noreply.github.com>
2021-08-02 16:05:56 +0600

    code #14 : Added CSS to the form
    Added the bootstrap class ‘form-control’, and some place holders to the form to make it look good.

--- Code/project/accounts/forms.py
+++ Code/project/accounts/forms.py
@@ -6,7 +6,7 @@ class RegistrationForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput(attrs={
 		'placeholder': 'Enter Password',
 		}))
-
+	
 	confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
 		'placeholder': 'Confirm Password',
 		}))
@@ -14,3 +14,15 @@ class RegistrationForm(forms.ModelForm):
 	class Meta:
 		model = Account
 		fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+
+
+	def __init__(self, *args, **kwargs):
+		super(RegistrationForm, self).__init__(*args, **kwargs)
+
+		self.fields['first_name'].widget.attrs['placeholder'] = 'First Name'
+		self.fields['last_name'].widget.attrs['placeholder'] = 'Last Name'
+		self.fields['email'].widget.attrs['placeholder'] = 'Email'
+		self.fields['phone_number'].widget.attrs['placeholder'] = '+8801XX-XXXX-XXX'
+
+		for field in self.fields:
+			self.fields[field].widget.attrs['class'] = 'form-control'
