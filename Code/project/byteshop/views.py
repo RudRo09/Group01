db71291c918da48ee2ce70e44dd82b58fddd1809
RudRo09 <islamrudro09@gmail.com>
2021-08-05 20:11:13 +0600

    enhancement #13 : Added search functionalities
    Added code to byteshop/views.py for the search functionalities by fetching the searched keyword in the  GET request. Modified the navbar.html accordingly and added some jQuery script in the base.html to add the search autocomplete feature.
    
    jQuery API documentation: https://api.jqueryui.com/autocomplete/

--- Code/project/byteshop/views.py
+++ Code/project/byteshop/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+# test purpose import
+from django.http import JsonResponse
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -45,3 +49,32 @@ def product_detail(request, category_slug, product_slug):
 	}
 
 	return render(request, 'byteshop/product_detail.html', context)
+
+
+# search functionalities
+def search(request):
+	products = None
+	product_count = 0
+
+	if 'keyword' in request.GET:
+		keyword = request.GET['keyword']
+		if keyword:
+			products = Product.objects.order_by('created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+			product_count = products.count()
+
+	if 'term' in request.GET:
+		qs = Product.objects.filter(product_name__istartswith=request.GET.get('term'))
+		titles = list()
+			
+		for product in qs:
+			titles.append(product.product_name)
+
+		return JsonResponse(titles, safe=False)
+
+
+	context = {
+		'products': products,
+		'product_count': product_count,
+	}
+
+	return render(request, 'byteshop/store.html', context)
