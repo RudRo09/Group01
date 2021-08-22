696fcbc02bca3788b6aa7e85fd9938a6ef14c5c3
Shadmanulislam11 <58678589+Shadmanulislam11@users.noreply.github.com>
2021-08-22 23:24:18 +0600

    enhancement #58 : Added forgot password feature
    Added forgot_password and reset_password view to accounts/views.py, and added their urls in the urls.py file.

--- Code/project/accounts/views.py
+++ Code/project/accounts/views.py
@@ -131,3 +131,36 @@ def activate(request, uidb64, token):
 @login_required(login_url='login')
 def dashboard(request):
 	return render(request, 'accounts/dashboard.html')
+
+
+def forgot_password(request):
+	if request.method == 'POST':
+		email = request.POST['email']
+		if Account.objects.filter(email=email).exists():
+			user = Account.objects.get(email__exact=email)
+
+			# reset password email
+			current_site = get_current_site(request)
+			mail_subject = 'Password Reset'
+			message = render_to_string('accounts/reset_password_email.html', {
+				'user': user,
+				'domain': current_site,
+				'uid': urlsafe_base64_encode(force_bytes(user.pk)),			# encoding primary key
+				'token': default_token_generator.make_token(user),
+				})
+			email_to = email
+			send_email = EmailMessage(mail_subject, message, to=[email_to])
+			send_email.send()
+
+			messages.success(request, 'Password reset email has been sent to your email address.')
+			return redirect('login')
+
+		else:
+			messages.error(request, 'Account does not exist!')
+			return redirect('forgot_password')
+
+	return render(request, 'accounts/forgot_password.html')
+
+
+def reset_password_validate(request):
+	return HttpResponse('Working!')
