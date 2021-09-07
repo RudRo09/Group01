4717313003d23bc8ab8306e0a122d96b58b5652f
SadiaShafaa <eleutheromania98.99@gmail.com>
2021-09-08 00:00:49 +0600

    code #18 : Added test code for payment
    Added code for integrating payment functionality.

--- Code/project/orders/views.py
+++ Code/project/orders/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def payment(request):
+ def confirm_payment(request):
+	if request.method == 'POST':
+		form = PaymentForm(request.POST)
+ 		if form.is_valid():
+ 			payment_data = Payment()
+			payment_data.payment_id = form.cleaned_data['payment_id']
+			payment_data.save()
 
-    return render(request, 'orders/payment.html', context)
+ 			return redirect('byteshop')
+
+ 	else:
+		return render(request, 'orders/confirm_payment.html')
